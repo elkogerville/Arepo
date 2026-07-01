7aaa50611639e4e8f817f17c7094930d5ed823f1
elkogerville <elkogerville@gmail.com>
2026-07-01 17:48:23 +0200

    rm gmp related definitions from voronoi.h (moved to utils/mpz_extension.h)

--- src/mesh/voronoi/voronoi.h
+++ src/mesh/voronoi/voronoi.h
@@ -32,8 +32,6 @@
 #ifndef HAVE_H_VORONOI
 #define HAVE_H_VORONOI
 
-#include <gmp.h>
-
 #define STACKSIZE_TETRA 10000
 #define MIN_ALLOC_NUMBER 1000
 #define ALLOC_INCREASE_FACTOR 1.1
@@ -46,14 +44,8 @@
 
 #if USEDBITS > 31
 typedef signed long long int IntegerMapType;
-void MY_mpz_set_si(mpz_t dest, signed long long int val);
-void MY_mpz_mul_si(mpz_t prod, mpz_t mult, signed long long int val);
-void MY_mpz_sub_ui(mpz_t prod, mpz_t mult, unsigned long long int val);
 #else /* #if USEDBITS > 31 */
 typedef signed long int IntegerMapType;
-#define MY_mpz_set_si mpz_set_si
-#define MY_mpz_mul_si mpz_mul_si
-#define MY_mpz_sub_ui mpz_sub_ui
 #endif /* #if USEDBITS > 31 #else */
 
 #define DOUBLE_to_VORONOIINT(y) ((IntegerMapType)(((*((long long *)&y)) & 0xFFFFFFFFFFFFFllu) >> (52 - USEDBITS)))
