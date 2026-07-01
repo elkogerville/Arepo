719b0b04d9a75e07a2f40676231ddce7f244aa03
elkogerville <elkogerville@gmail.com>
2026-07-01 17:54:52 +0200

    docs for mpz_extension.h

--- src/utils/mpz_extension.h
+++ src/utils/mpz_extension.h
@@ -1,5 +1,32 @@
-/*
- * If included, must be included after voronoi.h
+/*!
+ * \copyright   This file is part of the public version of the AREPO code.
+ * \copyright   Copyright (C) 2009-2019, Max-Planck Institute for Astrophysics
+ * \copyright   Developed by Volker Springel (vspringel@MPA-Garching.MPG.DE) and
+ *              contributing authors.
+ * \copyright   Arepo is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ *
+ *              Arepo is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *              GNU General Public License for more details.
+ *
+ *              A copy of the GNU General Public License is available under
+ *              LICENSE as part of this program.  See also
+ *              <https://www.gnu.org/licenses/>.
+ *
+ * \file        src/utils/mpz_extension.h
+ * \date        05/2018
+ * \brief       Header for mpz functions; **if included, must be included after voronoi.h**
+ * \details     Integer arithmetic used by Voronoi mesh construction.
+ *
+ *
+ * \par Major modifications and contributions:
+ *
+ * - DD.MM.YYYY Description
+ * - 29.05.2018 Prepared file for public release -- Rainer Weinberger
  */
 
 #ifndef MPZ_EXTENSION_H
